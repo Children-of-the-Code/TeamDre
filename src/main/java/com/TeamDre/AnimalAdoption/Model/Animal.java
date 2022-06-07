6c2e00e73a8a34dcbf2e6b1e53ca328031f88105
Fourpointstar <andreas.margarites@gmail.com>
2022-06-07 14:16:57 -0400

    Database up and running and can get all animals/add animal to the list

--- src/main/java/com/TeamDre/AnimalAdoption/Model/Animal.java
+++ src/main/java/com/TeamDre/AnimalAdoption/Model/Animal.java
@@ -1,8 +1,12 @@
 package com.TeamDre.AnimalAdoption.Model;
 import lombok.*;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
+
 
 
 @AllArgsConstructor
@@ -22,6 +26,7 @@ public class Animal {
     @Column
     private int age;
     @Column
+    //@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
     private Date date_added;
     @Column
     private String temperament;
