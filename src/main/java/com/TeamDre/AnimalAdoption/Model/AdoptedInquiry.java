9ca3e3e896c0505ce6469255f0b3a372e69e1951
Lysette <lysette409@revature.net>
2022-06-08 13:31:53 -0500

    configured AdoptedInquiry class

--- src/main/java/com/TeamDre/AnimalAdoption/Model/AdoptedInquiry.java
+++ src/main/java/com/TeamDre/AnimalAdoption/Model/AdoptedInquiry.java
@@ -1,4 +1,35 @@
 package com.TeamDre.AnimalAdoption.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class AdoptedInquiry {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int inquiry_id;
+    @Column
+    private Status status;
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name="animal_id", nullable = false)
+    private Animal animal;
+
+    public enum Status{
+        Pending,
+        Approved,
+        Denied
+    }
+
 }
