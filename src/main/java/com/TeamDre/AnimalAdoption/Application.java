4996e87d0ded0dae8cae189576435af296f04057
Fourpointstar <81208827+Fourpointstar@users.noreply.github.com>
2022-06-20 12:50:25 -0400

    updated

--- src/main/java/com/TeamDre/AnimalAdoption/Application.java
+++ src/main/java/com/TeamDre/AnimalAdoption/Application.java
@@ -2,9 +2,12 @@ package com.TeamDre.AnimalAdoption;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
     public static void main (String[] args) {
         try {
